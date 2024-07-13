f9c6a0a164a77499b19fde6ccbd1000c1a0b54a1
Jahongir <bahromovam843@gmail.com>
2024-07-13 10:49:34 -0800

    8-dars
    malumot turlari 2ga bulinadi, value types va reference malumot turlari. value typesni primitivega reference typesni objektga tenglasek buladi. misol uchun primitiveni qiymatidan nusxa olinadi , obyektni esa ko'rsatkichidan nusxa olinadi.

---
+++ 8dars/app.js
@@ -0,0 +1,22 @@
+// o'zgaruvchilarda
+
+let a = 5;
+let b = a;
+
+a = 10;
+
+console.log(a); // 10
+console.log(b); // 5
+
+
+
+// obyektlarda
+
+let x = { value: 10 };
+let y = x;
+
+x.value = 20;
+
+console.log(x); // { value: 20 }
+console.log(y); // { value: 20 }
+
